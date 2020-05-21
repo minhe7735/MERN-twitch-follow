1940af1fe2fd1058e078210d7424b3dfcb96c6cd
Min He <minhe@buffalo.edu>
2020-05-21 19:03:50 -0400

    df

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const MongoClient = require("mongodb");
+const MongoClient = require("mongodb").MongoClient;
 //const flash = require("express-flash");
 const express = require("express");
 const helmet = require("helmet");
