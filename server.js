aa6fd7d7cce4895d6e511b9f1dc4ce58faba4847
Min He <minhe@buffalo.edu>
2020-05-23 11:03:17 -0400

    testqweqwe

--- server.js
+++ server.js
@@ -46,15 +46,14 @@ MongoClient.connect(
         }
     }
 );
-// app.use("/", apiRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname + "/client", "build", "index.html"));
-// });
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
