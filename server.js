f0053830f214b9b183fa1b7dec03afe9e7d731ee
Min He <minhe@buffalo.edu>
2020-05-23 12:04:48 -0400

    testf

--- server.js
+++ server.js
@@ -49,9 +49,9 @@ MongoClient.connect(
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
